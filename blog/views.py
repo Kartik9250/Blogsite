0b3a505ae109a545f39a06314b43d7fef3027f1f
Kartik <kartikrana9250@gmail.com>
2022-02-03 07:04:05 +0530

    Added database integration

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'Jan 27, 2022'
-	},
-	{
-		'author': 'Waters',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'Jan 28, 2022'
-	},
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
